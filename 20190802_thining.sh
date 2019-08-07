c9fa800254669b7dcc333f319739fc116c49bfda
澁田謙太 <kenta@kentanoMacBook-Air.local>
2019-08-07 23:14:27 +0900

    correct output format

--- 20190802_thining.sh
+++ 20190802_thining.sh
@@ -3,4 +3,6 @@ echo ファイルのパスを入力:
 read file_name
 #echo 何行ごとにとりだす:
 #read line_num 
-awk '{if(($2!~"nan")&&(NR%1000==1)){printf "%23.15e %23.15e %23.15e %23.15e %23.15e \n", $1, $2, $3, $4, $5 > "test.txt"}}' $file_name
+#awk '{if(($2!~"nan")&&(NR%10==1)){printf "%e %e %e %e %e \n", $1, $2, $3, $4, $5 > "test.txt"}}' $file_name
+awk '{if(($2!~"nan")&&(NR%1000==1)){print $1, $2, $3, $4, $5 > "test.txt"}}' $file_name
+
