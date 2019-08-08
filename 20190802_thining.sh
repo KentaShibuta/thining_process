a17aaa30d80ba9d609fce7db952218672d8a4df1
澁田謙太 <kenta@kentanoMacBook-Air.local>
2019-08-08 11:54:56 +0900

    add output as csv format

--- 20190802_thining.sh
+++ 20190802_thining.sh
@@ -4,5 +4,9 @@ read input_file_name
 output_file_name=`date +"%Y%m%d%H%M%S"`
 #awk '{if(($2!~"nan")&&(NR%10==1)){printf "%e %e %e %e %e \n", $1, $2, $3, $4, $5 > "test.txt"}}' $file_name
 echo $output_file_name
-awk '{if(($2!~"nan")&&(NR%1000==1)){print $1, $2, $3, $4, $5>"'"${output_file_name}"'.txt"}}' $input_file_name
-# 参考 https://tech-knygt51.com/bash_variables_in_awk/
+awk 'BEGIN{
+		OFS = ","
+	}
+	{if(($2!~"nan")&&(NR%1000==1))
+		{print $1, $2, $3, $4, $5>"'"${output_file_name}"'.csv"}
+	}' $input_file_name
